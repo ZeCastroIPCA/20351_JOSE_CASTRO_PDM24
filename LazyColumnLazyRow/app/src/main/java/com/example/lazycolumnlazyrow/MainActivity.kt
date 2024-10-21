3ee8dc26b3836aebc07c3b883a6354ce065c5a76
Zé Castro <a20351@alunos.ipca.pt>
2024-10-21 12:07:36 +0100

    Lazy Loading

---
+++ LazyColumnLazyRow/app/src/main/java/com/example/lazycolumnlazyrow/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.example.lazycolumnlazyrow
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.lazycolumnlazyrow.ui.theme.LazyColumnLazyRowTheme
+
+class MainActivity : ComponentActivity() {
+    val brandsList = listOf("Apple", "Microsoft", "Nvidia", "Intel", "AMD")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            LazyColumnLazyRowTheme {
+                LazyLoading(brandsList)
+            }
+        }
+    }
+}
+
+@Composable
+fun LazyLoading(itemsList: List<String>) {
+    LazyColumn {
+        items(itemsList) {
+            item -> Text(text = item.toString())
+        }
+    }
+}
