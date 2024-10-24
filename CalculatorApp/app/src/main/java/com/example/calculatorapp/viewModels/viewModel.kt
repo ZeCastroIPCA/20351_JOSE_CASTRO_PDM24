f4393f8e4097d741ab4ec5cb1334acb76abf8a34
Zé Castro <a20351@alunos.ipca.pt>
2024-10-24 20:42:33 +0100

    removed attempt to use models/ui/viewModel to build calculator

--- CalculatorApp/app/src/main/java/com/example/calculatorapp/viewModels/viewModel.kt
+++
@@ -1,9 +0,0 @@
-package com.example.calculatorapp.viewModels
-
-import androidx.compose.runtime.mutableStateOf
-import androidx.lifecycle.ViewModel
-import com.example.calculatorapp.models.CalculatorBrain
-
-class viewModel : ViewModel() {
-    val brain = mutableStateOf(CalculatorBrain())
-}
